7dbd39b4e14bd4043c7168902bc84fceaea3cea6
cool.li <liang@vpgame.cn>
2019-07-05 17:40:40 +0800

    '分离logiN'

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 //导入english_words包
 import 'package:english_words/english_words.dart';
+//导入login 包
+import './login.dart';
 
 void main() => runApp(MyApp());
 
@@ -98,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 //导航到新路由
                 Navigator.push(context,
                     new MaterialPageRoute(builder: (context) {
-                  return new NewRoute();
+                  return new Login();
                 }));
               },
             ),
@@ -145,87 +147,6 @@ class Echo extends StatelessWidget {
       child: Container(
         color: backgroundColor,
         child: Text(text),
-      ),
-    );
-  }
-}
-
-//新建路由
-class NewRoute extends StatefulWidget {
-  @override
-  _FormTestRouteState createState() => new _FormTestRouteState();
-}
-
-class _FormTestRouteState extends State<NewRoute> {
-  TextEditingController _unameController = new TextEditingController();
-  TextEditingController _pwdController = new TextEditingController();
-  GlobalKey _formKey = new GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Form Test"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-        child: Form(
-          key: _formKey, //设置globalKey，用于后面获取FormState
-          autovalidate: true, //开启自动校验
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                  autofocus: true,
-                  controller: _unameController,
-                  decoration: InputDecoration(
-                      labelText: "用户名",
-                      hintText: "用户名或邮箱",
-                      icon: Icon(Icons.person)),
-                  // 校验用户名
-                  validator: (v) {
-                    return v.trim().length > 0 ? null : "用户名不能为空";
-                  }),
-              TextFormField(
-                  controller: _pwdController,
-                  decoration: InputDecoration(
-                      labelText: "密码",
-                      hintText: "您的登录密码",
-                      icon: Icon(Icons.lock)),
-                  obscureText: true,
-                  //校验密码
-                  validator: (v) {
-                    return v.trim().length > 5 ? null : "密码不能少于6位";
-                  }),
-              // 登录按钮
-              Padding(
-                padding: const EdgeInsets.only(top: 28.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(15.0),
-                        child: Text("登录"),
-                        color: Theme.of(context).primaryColor,
-                        textColor: Colors.white,
-                        onPressed: () {
-                          //在这里不能通过此方式获取FormState，context不对
-                          //print(Form.of(context));
-
-                          // 通过_formKey.currentState 获取FormState后，
-                          // 调用validate()方法校验用户名密码是否合法，校验
-                          // 通过后再提交数据。
-                          if ((_formKey.currentState as FormState).validate()) {
-                            //验证通过提交数据
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
